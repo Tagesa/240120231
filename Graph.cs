d3e8fcda6398df53c9cb082e486862e5f2a73b55
Tagesa <123443156+Tagesa@users.noreply.github.com>
2023-01-25 10:23:59 +0400

    Add files via upload

---
+++ Graph.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graf
+{
+    class Vertex
+    {
+        public int vertex;
+        public List<int> ConnectionList;
+        public Vertex(int Vertex, List<int> ConnectionList) {
+            this.vertex = Vertex;
+            this.ConnectionList = ConnectionList;
+        }
+
+        public void Printt() {
+            Console.Write("Вершина: {0}, смежные вершины: [ ", vertex.ToString());
+            foreach (var elem in ConnectionList)
+            {
+                Console.Write(elem + " ");
+            }
+            Console.Write("]");
+            Console.WriteLine();
+        }
+        
+    }
+    class Graph
+    {
+        public List<Vertex> list = new List<Vertex>();
+        public void AddVertex(Vertex edit)
+        {
+            list.Add(edit);
+        }
+
+        public void DeliteVertex(Vertex edit)
+        {
+            list.Remove(edit);
+        }
+
+        public void AddConnection(Vertex edit, int Connection) {
+            var iSFind = list.Find(x => x.vertex == edit.vertex);
+            if (iSFind != null) {
+                edit.ConnectionList.Add(Connection);                
+            }
+            else {
+                Console.WriteLine("Нет смежности");
+            }            
+        }
+
+        public void DeleteConnection(Vertex edit, int Connection)
+        {
+            var iSFind = list.Find(x => x.vertex == edit.vertex);
+            if (iSFind != null)
+            {
+                var Number = edit.ConnectionList.Find(x => x == Connection);
+                if (Number != null)
+                {
+                    edit.ConnectionList.Remove(Connection);
+                }
+                else
+                {
+                    Console.WriteLine("Данная смежность нет");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Нет смежности");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1;
+            for (int i = 0; i < 100; i++)
+            {
+                a = Menu();
+                if (a == 0) {
+                    return;
+                }
+            }
+
+            Console.ReadKey();
+        }
+
+        public static int Menu() {
+            //Создание вершин БАЗОВЫЙ
+            List<int> k1 = new List<int>() { 2, 3, 4 };
+            Vertex a1 = new Vertex(1, k1);
+
+            List<int> k2 = new List<int>() { 3, 4 };
+            Vertex a2 = new Vertex(2, k2);
+
+            List<int> k3 = new List<int>() { 4 };
+            Vertex a3 = new Vertex(3, k3);
+
+            List<int> k4 = new List<int>() { 1, 2 };
+            Vertex a4 = new Vertex(4, k4);
+
+            Graph Gr = new Graph();
+
+            Gr.AddVertex(a1);
+            Gr.AddVertex(a2);
+            Gr.AddVertex(a3);
+            Gr.AddVertex(a4);
+
+
+            Console.WriteLine("Старт");
+            Console.WriteLine("1. Вывести граф \n2. Добавить вершину \n3. Удалить вершину \n4. Добавить смежность \n5. Удалить смежность");
+
+            int rez = Convert.ToInt32(Console.ReadLine());
+            if (rez == 1)
+            {
+                foreach (var item in Gr.list)
+                {
+                    item.Printt();
+                }
+            }
+            else if (rez == 2)
+            {
+                Console.WriteLine("Введите вершину");
+                int Value = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Введите смежноcть");
+                int Value1 = Convert.ToInt32(Console.ReadLine());
+                Gr.AddVertex(new Vertex(Value,new List<int> { Value1 }));
+                Console.WriteLine("После добавления");
+                foreach (var item in Gr.list)
+                {
+                    item.Printt();
+                }
+            }
+            else if (rez == 3)
+            {
+                Console.WriteLine("Введите вершину");
+                int Value = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("После удаления вершины {0}", Value.ToString());
+                var ver = Gr.list.Find(x => x.vertex == Value);
+                Gr.DeliteVertex(ver);
+                foreach (var item in Gr.list)
+                {
+                    item.Printt();
+                }
+            }
+            else if (rez == 4)
+            {
+                Console.WriteLine("Введите вершину");
+                int Value = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Введите смежноcть");
+                int Value1 = Convert.ToInt32(Console.ReadLine());
+                var ver = Gr.list.Find(x => x.vertex == Value);
+                Gr.AddConnection(ver, Value1);
+                Console.WriteLine("После добавления ребра вершине {0}", Value.ToString());
+
+                foreach (var item in Gr.list)
+                {
+                    item.Printt();
+                }
+            }
+            else if (rez == 5)
+            {
+                Console.WriteLine("Введите вершину");
+                int Value = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Введите смежноcть");
+                int Value1 = Convert.ToInt32(Console.ReadLine());
+                var ver = Gr.list.Find(x => x.vertex == Value);
+
+                Gr.DeleteConnection(ver, Value1);
+                Console.WriteLine("После удаления ребра вершине {0}", Value.ToString());
+
+                foreach (var item in Gr.list)
+                {
+                    item.Printt();
+                }
+            }
+            else
+            {
+                return 0;
+            }
+            return 2;
+        }
+    }
+}
